a725f0779208482d6c7574768ea0359a9f38ddd5
Nutravee <nutjung2353@gmail.com>
2024-09-05 15:22:42 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+console.log("This is my script file from Javascript");//แสดงผลในคอนโซล
+document.getElementById("text").innerHTML = "Nutravee Prongsuntea";//แสดงผลหน้าเว็บ
+document.write("Hello document write");
+window.alert("welcome to my page");//แสดงalert
