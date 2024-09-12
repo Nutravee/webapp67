533dfd392a4dbc479dd328336d256b376f341a12
Nutravee <nutjung2353@gmail.com>
2024-09-12 15:44:25 +0700

    Add files via upload

---
+++ phpbasic/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP";
+        echo "<br>";
+        echo "this is my first time to write PHP Languase";
+    echo "<br>";
+    echo "<br>";
+    define("MINSIZE",50);
+    echo MINSIZE;
+    ?>
+</body>
+</html>
---
+++ phpbasic/function26.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function writeMessage() //create function
+        {
+            echo "this is first function for write Message";
+            echo "<br>";
+            echo "Have a nice program";
+        }
+        
+        //call function
+        writeMessage();
+    ?>
+</body>
+</html>
